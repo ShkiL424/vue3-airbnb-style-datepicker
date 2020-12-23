70399240ce0d27ce2f6482aa918a01d6115c455f
Dmitriy Shkil <ShkiL424@gmail.com>
2020-12-23 23:13:55 +0200

    init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Vue3AirbnbStyleDatepicker from '@/vue3-airbnb-style-datepicker.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    Vue3AirbnbStyleDatepicker
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue3-airbnb-style-datepicker />
+  </div>
+</template>
